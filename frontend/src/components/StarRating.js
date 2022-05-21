78054bcb947acdb9dda3ce4fc18cd19f9ba0c701
hveladzic2 <hveladzic2@etf.unsa.ba>
2022-05-21 13:14:02 +0200

    access to the rating form is only available to logged-in users

--- frontend/src/components/StarRating.js
+++ frontend/src/components/StarRating.js
@@ -8,14 +8,20 @@ class StarRating extends React.Component {
 
  constructor(props) {
         super(props);
-        const userId = AuthService.getCurrentUser().userId;
         this.state = {
           rating: 0, 
           comment:'',
           recipeId : "a763442e-fdb7-4ffd-834e-56bc135aa825",
-          userId : userId
+          userId : '',
+          isLoggedIn : false
         };
  }
+ componentDidMount() {
+  if(AuthService.getCurrentUser() != null) {
+      this.setState({isLoggedIn : true})
+      this.setState({ userId : AuthService.getCurrentUser().userId})
+  }
+}
 
  handleRating = (rate) => {
     this.setState({rating : rate/(2*10)})
@@ -50,26 +56,32 @@ class StarRating extends React.Component {
   }
 
   render() {
-      return <form onSubmit={this.submitNew}>
-                <label>Your Rating</label><br/>
-                <Rating 
-                  size="25"
-                  fillColor='#ff6127'
-                  required="required"
-                  onClick={this.handleRating} 
-                  ratingValue={this.state.rating}  
-                /> <br/>
-                <textarea 
-                  className='review-textarea' 
-                  required="required"
-                  maxLength="255"
-                  name="comment"
-                  placeholder='Write your review here'
-                  onChange={this.onChange} 
-                  value={this.state.comment === null ? '' : this.state.comment }
-                /><br/>
-                <button type='submit'>POST</button> 
-            </form>      
+    {
+      if(this.state.isLoggedIn){
+        return <form onSubmit={this.submitNew}>
+        <label>Your Rating</label><br/>
+        <Rating 
+          size="25"
+          fillColor='#ff6127'
+          required="required"
+          onClick={this.handleRating} 
+          ratingValue={this.state.rating}  
+        /> <br/>
+        <textarea 
+          className='review-textarea' 
+          required="required"
+          maxLength="255"
+          name="comment"
+          placeholder='Write your review here'
+          onChange={this.onChange} 
+          value={this.state.comment === null ? '' : this.state.comment }
+        /><br/>
+        <button type='submit'>POST</button> 
+    </form>   
+      } else{
+        return <div></div>
+      }
+    }   
     }
 }
 export default StarRating;

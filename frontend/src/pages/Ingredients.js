00f8b4c8675576e4a25f05e955e141620bf02d20
hveladzic2 <hveladzic2@etf.unsa.ba>
2022-05-26 15:56:14 +0200

    added test images to create initial data in the database

--- frontend/src/pages/Ingredients.js
+++ frontend/src/pages/Ingredients.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../style/Ingredients.css';
 import IngredientService from '../services/IngredientService';
 import PictureService from '../services/PictureService';
 import axios from 'axios';
 import AuthService from '../services/AuthService';
-import {confirmAlert} from "react-confirm-alert";
+import { confirmAlert } from "react-confirm-alert";
 
 
 class Ingredients extends Component {
@@ -29,11 +29,11 @@ class Ingredients extends Component {
 
     componentDidMount() {
         IngredientService.getIngredients().then((res) => {
-            this.setState({ingredients: res.data});
-            this.setState({allingredients: res.data});
+            this.setState({ ingredients: res.data });
+            this.setState({ allingredients: res.data });
         });
         PictureService.getIngredientPictures().then((res) => {
-            this.setState({pictures: res.data});
+            this.setState({ pictures: res.data });
         })
     }
 
@@ -41,9 +41,9 @@ class Ingredients extends Component {
         let list = this.state.ingredients;
         if (e != '' && e != null) {
             list = this.state.ingredients.filter((item) => (item.name.toLowerCase().includes(e.toLowerCase())));
-            this.setState({ingredients: list})
+            this.setState({ ingredients: list })
         } else {
-            this.setState({ingredients: this.state.allingredients})
+            this.setState({ ingredients: this.state.allingredients })
         }
     }
 
@@ -51,8 +51,8 @@ class Ingredients extends Component {
         const re = /^[0-9\b]+$/;
         return this.state.values.map((el, i) =>
             <div key={i}>
-                <button style={{marginRight: "2%"}} className='add-button' type='button' value='remove'
-                        onClick={this.removeClick.bind(this, i)}><i className='fas fa-trash-alt'></i></button>
+                <button style={{ marginRight: "2%" }} className='add-button' type='button' value='remove'
+                    onClick={this.removeClick.bind(this, i)}><i className='fas fa-trash-alt'></i></button>
 
                 <input
                     required="required"
@@ -67,9 +67,9 @@ class Ingredients extends Component {
                     <option>g</option>
                     <option>ml</option>
                 </select>
-                <label className="add-ingredient" style={{marginLeft: "20%"}}> <img className='ingredient-image'
-                                                                                    src={'data:image/jpeg;base64,' + this.map.get(this.labela[i])}
-                                                                                    alt=''/> {this.labela[i]}</label>
+                <label className="add-ingredient" style={{ marginLeft: "20%" }}> <img className='ingredient-image'
+                    src={'data:image/jpeg;base64,' + this.map.get(this.labela[i])}
+                    alt='' /> {this.labela[i]}</label>
             </div>
         )
     }
@@ -77,11 +77,11 @@ class Ingredients extends Component {
     handleChange(i, event) {
         let values = [...this.state.values];
         values[i] = event.target.value;
-        this.setState({values});
+        this.setState({ values });
     }
 
     addClick(ingredient, slika) {
-        this.setState(prevState => ({values: [...prevState.values, '']}))
+        this.setState(prevState => ({ values: [...prevState.values, ''] }))
         this.labela[this.br] = ingredient
         this.map.set(ingredient, slika);
         this.br = this.br + 1
@@ -95,7 +95,7 @@ class Ingredients extends Component {
         }
         this.br = this.br - 1
         values.splice(i, 1)
-        this.setState({values});
+        this.setState({ values });
     }
 
     handleSubmit(event) {
@@ -119,11 +119,11 @@ class Ingredients extends Component {
                         recipeID: this.state.recipeID,
                         ingredientRecipeID: this.state.ingredientIdMap.get(this.labela[i])
                     }), {
-                        headers: {
-                            'Authorization': 'Bearer ' + AuthService.getCurrentUser().token,
-                            'Content-Type': 'application/json'
-                        }
-                    });
+                    headers: {
+                        'Authorization': 'Bearer ' + AuthService.getCurrentUser().token,
+                        'Content-Type': 'application/json'
+                    }
+                });
             }
             confirmAlert({
                 title: 'NOTIFICATION',
@@ -150,9 +150,9 @@ class Ingredients extends Component {
         return (
             <div>
                 <div>
-                    <h2 style={{marginLeft: "40%"}} className='h2-style'>FoodHaven</h2>
+                    <h2 style={{ marginLeft: "40%" }} className='h2-style'>FoodHaven</h2>
                     <a className='a-back' href='./RecipeInfo'> <i className="fas fa-angle-left"></i> Back</a>
-                    <h1 style={{marginLeft: "5%"}}>Ingredients</h1>
+                    <h1 style={{ marginLeft: "5%" }}>Ingredients</h1>
                 </div>
                 <input
                     className='search-input'
@@ -161,29 +161,31 @@ class Ingredients extends Component {
                     placeholder="Search ... "
                 />
                 <span className='search-span'>
-          <i className="fas fa-search"/>
-        </span>
-                <br/>
-                <form style={{marginLeft: "5%"}} onSubmit={this.handleSubmit}>
+                    <i className="fas fa-search" />
+                </span>
+                <br />
+                <form style={{ marginLeft: "5%" }} onSubmit={this.handleSubmit}>
                     {this.state.ingredients.map(
                         ingredient => (
                             this.state.ingredientIdMap.set(ingredient.name, ingredient.id),
-                                <div key={ingredient.id}>
-                                    <button key={ingredient.id + "1"} className="add-button" type='button' value='add'
-                                            onClick={this.addClick.bind(this, ingredient.name, this.state.pictureMap.get(ingredient.ingredientPicture))}>
-                                        <i className='fas fa-plus'></i></button>
-                                    <label key={ingredient.id + "2"} className="add-ingredient"
-                                           onChange={this.handleChange.bind(this)}><img className='ingredient-image'
-                                                                                        src={'data:image/jpeg;base64,' + this.state.pictureMap.get(ingredient.ingredientPicture)}
-                                                                                        alt=''/> {ingredient.name}
-                                    </label> <br/>
-                                </div>
+                            <div style={{ float: 'left', width:'30%'}} key={ingredient.id}>
+                                <button key={ingredient.id + "1"} className="add-button" type='button' value='add'
+                                    onClick={this.addClick.bind(this, ingredient.name, this.state.pictureMap.get(ingredient.ingredientPicture))}>
+                                    <i className='fas fa-plus'></i></button>
+                                <label key={ingredient.id + "2"} className="add-ingredient"
+                                    onChange={this.handleChange.bind(this)}><img className='ingredient-image'
+                                        src={'data:image/jpeg;base64,' + this.state.pictureMap.get(ingredient.ingredientPicture)}
+                                        alt='' /> {ingredient.name}
+                                </label>
+                            </div>
                         ))}
-                    <br/><h4 style={{marginLeft: "4%"}}>Quantity</h4><br/>
-                    {this.createUI()} <br/> <br/>
-                    <button type='submit'>
-                        <i className='fas fa-save'></i> Save ingredients
-                    </button>
+                    <div style={{clear:'left'}}>
+                        <br/><h4 style={{marginLeft:'6%'}}>Quantity</h4><br />
+                        {this.createUI()} <br/>
+                        <button type='submit'>
+                            <i className='fas fa-save'></i> Save ingredients
+                        </button>
+                    </div>
                 </form>
             </div>
         );
--- ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/IngredientServiceApplication.java
+++ ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/IngredientServiceApplication.java
@@ -33,31 +33,64 @@ public class IngredientServiceApplication {
         SpringApplication.run(IngredientServiceApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner demo(PictureService pictureService, IngredientService ingredientService,
-//                                  IngredientRecipeService ingredientRecipeService) {
-//        return (args) -> {
-//
-//            MultipartFile file = null;
+    @Bean
+    public CommandLineRunner demo(PictureService pictureService, IngredientService ingredientService,
+                                  IngredientRecipeService ingredientRecipeService) {
+        return (args) -> {
 //            try {
-//                file = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/image.jpg")));
-//                UUID p1=pictureService.create(file);
-//                UUID p2=pictureService.create(file);
-//                UUID p3=pictureService.create(file);
-//                UUID uuid = UUID.fromString("04b03880-5b56-4464-a466-a150958c32f7");
-//                UUID r1 = ingredientService.create(new IngredientDTO("Chicken", 200,
-//                        0, 12, 3, 12, "gram", p1));
-//                UUID r2 = ingredientService.create(new IngredientDTO("Milk", 200,
-//                        4, 0, 3, 5, "liter", p2));
-//                UUID r3 = ingredientService.create(new IngredientDTO("Strawberry",
-//                        200, 10, 2, 0, 4, "gram", p3));
-//                ingredientRecipeService.create(new IngredientRecipeDTO(1, uuid, r1));
+//                MultipartFile file1 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/sugar.jpg")));
+//                MultipartFile file2 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/milk.jpg")));
+//                MultipartFile file3 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/butter.jpg")));
+//                MultipartFile file4 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/cheese.jpg")));
+//                MultipartFile file5 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/chicken.jpg")));
+//                MultipartFile file6 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/chocolate.jpg")));
+//                MultipartFile file7 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/mushrooms.jpg")));
+//                MultipartFile file8 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/oil.jpg")));
+//                MultipartFile file9 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/strawberry.jpg")));
+//                MultipartFile file10 = new MockMultipartFile("image.jpg", new FileInputStream(new File("ingredient-service/src/main/java/ba/unsa/etf/nwt/ingredient_service/image/tomato.jpg")));
+//
+//                UUID p1=pictureService.create(file1);
+//                UUID p2=pictureService.create(file2);
+//                UUID p3=pictureService.create(file3);
+//                UUID p4=pictureService.create(file4);
+//                UUID p5=pictureService.create(file5);
+//                UUID p6=pictureService.create(file6);
+//                UUID p7=pictureService.create(file7);
+//                UUID p8=pictureService.create(file8);
+//                UUID p9=pictureService.create(file9);
+//                UUID p10=pictureService.create(file10);
+//
+//                UUID uuid = UUID.fromString("83f7b480-c905-4e24-9b0c-b3038fc4b706");
+//
+//                UUID r1 = ingredientService.create(new IngredientDTO("Sugar", 15,
+//                        0, 4, 0, 0, "gram", p1));
+//                UUID r2 = ingredientService.create(new IngredientDTO("Milk", 149,
+//                        4, 12, 8, 8, "ml", p2));
+//                UUID r3 = ingredientService.create(new IngredientDTO("Butter",
+//                        102, 0, 0, 12, 0, "gram", p3));
+//                UUID r4 = ingredientService.create(new IngredientDTO("Cheese", 120,
+//                        4, 12, 3, 14, "gram", p4));
+//                UUID r5 = ingredientService.create(new IngredientDTO("Chicken", 120,
+//                        1, 0, 2, 26, "gram", p5));
+//                UUID r6 = ingredientService.create(new IngredientDTO("Chocolate",
+//                        216, 0, 25, 13, 2, "gram", p6));
+//                UUID r7 = ingredientService.create(new IngredientDTO("Mushrooms", 22,
+//                        3, 3, 0, 3, "gram", p7));
+//                UUID r8 = ingredientService.create(new IngredientDTO("Oil", 120,
+//                        0, 0, 14, 0, "ml", p8));
+//                UUID r9 = ingredientService.create(new IngredientDTO("Strawberry",
+//                        32, 8, 8, 0, 1, "gram", p9));
+//                UUID r10 = ingredientService.create(new IngredientDTO("Tomato", 18,
+//                        9, 4, 0, 1, "gram", p10));
+//
+//
+//                ingredientRecipeService.create(new IngredientRecipeDTO(1, uuid, r8));
 //                ingredientRecipeService.create(new IngredientRecipeDTO(2, uuid, r2));
 //                ingredientRecipeService.create(new IngredientRecipeDTO(3, uuid, r3));
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
-//
-//        };
-//    }
+
+        };
+    }
 }

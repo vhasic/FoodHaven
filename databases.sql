15da2d67d7488cf625fc3c95c8be555afa6d20a9
Vahidin <vhasic1@etf.unsa.ba>
2022-05-30 20:20:10 +0200

    Docker implementation

---
+++ databases.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS ingredientServiceDB;
+CREATE DATABASE IF NOT EXISTS reviewServiceDB;
+CREATE DATABASE IF NOT EXISTS recipeServiceDB;
+CREATE DATABASE IF NOT EXISTS userServiceDB;
+CREATE DATABASE IF NOT EXISTS systemEventsServiceDB;
+
+GRANT ALL ON ingredientServiceDB.* TO 'springuser'@'%';
+GRANT ALL ON reviewServiceDB.* TO 'springuser'@'%';
+GRANT ALL ON recipeServiceDB.* TO 'springuser'@'%';
+GRANT ALL ON userServiceDB.* TO 'springuser'@'%';
+GRANT ALL ON systemEventsServiceDB.* TO 'springuser'@'%';
+
